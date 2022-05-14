2e700e35d0bc4a3e8769fbc7f7e59eb2f0d4fd51
Arthur Marques <arthur.l2@hotmail.com>
2022-05-14 18:50:01 -0300

    Criando projeto e domain de category

---
+++ domain/src/test/java/com/fullcycle/admin/catalogo/domain/category/CategoryTest.java
@@ -0,0 +1,34 @@
+package com.fullcycle.admin.catalogo.domain.category;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryTest {
+
+    @Test
+    void givenAValidParams_whenCallNewCategory_thenInstantiateACategory() {
+        final var expectedName = "filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+
+        final var actualCategory = Category.newCategory(
+                expectedName,
+                expectedDescription,
+                expectedIsActive
+        );
+
+        assertAll(() -> {
+            assertNotNull(actualCategory);
+            assertNotNull(actualCategory.getId());
+
+            assertEquals(expectedName, actualCategory.getName());
+            assertEquals(expectedDescription, actualCategory.getDescription());
+            assertEquals(expectedIsActive, actualCategory.isActive());
+
+            assertNotNull(actualCategory.getCreatedAt());
+            assertNotNull(actualCategory.getUpdatedAt());
+            assertNull(actualCategory.getDeletedAt());
+        });
+    }
+}
---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/infrastructure/MainTest.java
@@ -0,0 +1,12 @@
+package com.fullcycle.admin.catalogo.infrastructure;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void testMain() {
+        Assertions.assertNotNull(new Main());
+    }
+}
