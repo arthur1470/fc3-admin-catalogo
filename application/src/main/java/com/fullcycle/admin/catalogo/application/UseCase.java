a18b972a57b863f3d8a9bb7d2fd208942d3d5627
Arthur Marques <arthur.l2@hotmail.com>
2022-05-15 11:06:51 -0300

    Criando estrutura base de usecase

--- application/src/main/java/com/fullcycle/admin/catalogo/application/UseCase.java
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/UseCase.java
@@ -1,6 +1,6 @@
 package com.fullcycle.admin.catalogo.application;
 
-import com.fullcycle.admin.catalogo.domain.category.Category;
+public abstract class UseCase<IN, OUT> {
 
-public class UseCase {
+    public abstract OUT execute(IN anIn);
 }
