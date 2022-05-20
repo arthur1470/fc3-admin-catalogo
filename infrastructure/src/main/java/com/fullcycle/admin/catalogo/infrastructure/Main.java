e16544d359e0d1ababd0c71051bc80a587b4b950
Arthur Marques <arthur.l2@hotmail.com>
2022-05-19 23:18:55 -0300

    Adicionando springboot no projeto

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/Main.java
@@ -1,4 +1,12 @@
 package com.fullcycle.admin.catalogo.infrastructure;
 
+import com.fullcycle.admin.catalogo.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(WebServerConfig.class, args);
+    }
 }
