09f04302805f5532852c1b2125bcaf1e7c0c60a9
Arthur Marques <arthur.l2@hotmail.com>
2022-07-04 23:39:33 -0300

    Testes integrados do metodo create no gateway de genre

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/genre/persistence/GenreJpaEntity.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/genre/persistence/GenreJpaEntity.java
@@ -7,6 +7,7 @@ import com.fullcycle.admin.catalogo.domain.genre.GenreID;
 import javax.persistence.*;
 import java.time.Instant;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -90,6 +91,12 @@ public class GenreJpaEntity {
 
     private void removeCategory(final CategoryID anId) {
         this.categories.remove(GenreCategoryJpaEntity.from(this, anId));
+    }
+
+    public List<CategoryID> getCategoryIds() {
+        return getCategories().stream()
+                .map(it -> CategoryID.from(it.getId().getCategoryId()))
+                .toList();
     }
 
     public String getId() {
